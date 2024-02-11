7d513cb7e0a39889e82e8a6cdd3e68c17a7b7137
AtharvaAnbhule <125785886+AtharvaAnbhule@users.noreply.github.com>
2024-02-11 20:00:28 +0530

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import ContactFormEmail from "@/email/contact-form-email";
 import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_eEDdFJZC_5XoH6bQkDz19xDBCgSxw4f7b");
 
 
 
